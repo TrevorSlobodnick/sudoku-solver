ea5d4a3f60012a115acfcc98f2238f721566baa0
TrevorSlobodnick <trevorslobodnick@gmail.com>
2022-08-25 22:31:36 -0400

    Added functions to get a box/row/column

--- classes.js
+++ classes.js
@@ -35,4 +35,31 @@ class Board{
     constructor(cells){
         this.cells = cells
     }
+
+    /**
+     * Get a specific row from the board
+     * @param {Number} num the row to get [1-9]
+     * @returns {Array<Cell>} the requested row
+     */
+    getRow(num){
+        return this.cells.filter(value => value.pos.row === num)
+    }
+
+    /**
+     * Get a specific column from the board
+     * @param {Number} num the column to get [1-9]
+     * @returns {Array<Cell>} the requested column
+     */
+    getColumn(num){
+        return this.cells.filter(value => value.pos.col === num)
+    }
+
+    /**
+     * Get a specific box from the board
+     * @param {Number} num the box to get [1-9]
+     * @returns {Array<Cell>} the requested box
+     */
+    geBox(num){
+        return this.cells.filter(value => value.box === num)
+    }
 }
